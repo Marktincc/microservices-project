50beba7b7449a730b85b252ca3d278867ffb4a3e
Marktin <marktincc@gmail.com>
2025-05-22 16:18:23 -0500

    feat: agregar implementación del servicio de ventas y controlador, incluyendo operaciones CRUD y autenticación JWT

--- script-create-database/ScriptDBconsultaDatosProductos.sql
+++ script-create-database/ScriptDBconsultaDatosProductos.sql
@@ -20,4 +20,34 @@ BEGIN
         suppliersdb.proveedores s ON p.proveedor_id_proveedor = s.id;
 END $$
 
+
+
+USE salesDb;
+DELIMITER $$
+
+CREATE PROCEDURE `get_sale_info`()
+BEGIN
+    SELECT
+        s.id AS id_venta,
+        s.fecha AS fecha_venta,
+        s.cantidad AS cantidad,
+        s.valor AS valor_total,
+
+        -- Info producto
+        p.nombre_producto AS productoNombre,
+
+
+        -- Info cliente
+        c.nombre AS clienteNombre
+
+
+    FROM ventas s
+             LEFT JOIN productsdb.productos p ON s.id_producto = p.id
+             LEFT JOIN customersdb.customers c ON s.id_usuario = c.id;
+END$$
+
 DELIMITER ;
+
+CALL get_sale_info();
+
+
